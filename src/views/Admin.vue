1a7f5a103497a8ae83237f1ab5ff58cfa044c8fd
BuildTools <unconfigured@null.spigotmc.org>
2022-05-17 17:20:37 -0600

    formatting

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,40 +1,51 @@
 <template>
-	<v-navigation-drawer permanent>
-		<v-container grid-list-xs align="space-between">
-			<v-row>
-				<v-col>
-					<router-link to="/">
-						<v-btn color="success"><i class="fa fa-arrow-circle-left" aria-hidden="true" /> Home
-						</v-btn>
-					</router-link>
-				</v-col>
-			</v-row>
-			<v-row>
-				<v-col>
-					<v-btn color="info" @click="active = 'staff'">Staff Menu</v-btn>
-				</v-col>
-			</v-row>
-			<v-row>
-				<v-col>
-					<v-btn color="info">Item Menu</v-btn>
-				</v-col>
-			</v-row>
-		</v-container>
-	</v-navigation-drawer>
-	<StaffMenu v-if="active == 'staff'" />
+  <v-navigation-drawer permanent>
+    <v-container grid-list-xs align="space-between">
+      <v-row>
+        <v-col>
+          <router-link to="/">
+            <v-btn color="error"
+              ><i class="fa fa-arrow-circle-left" aria-hidden="true" /> Home
+            </v-btn>
+          </router-link>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn
+            :color="active == 'staff' ? 'success' : 'info'"
+            @click="active = 'staff'"
+            >Staff Menu</v-btn
+          >
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn
+            :color="active == 'item' ? 'success' : 'info'"
+            @click="active = 'item'"
+            >Item Menu</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-navigation-drawer>
+  <StaffMenu v-if="active == 'staff'" />
+  <ItemMenu v-if="active == 'item'" />
 </template>
 
 <script>
-import StaffMenu from '../components/StaffMenu.vue';
-
+import StaffMenu from "../components/StaffMenu.vue";
+import ItemMenu from "../components/ItemMenu.vue";
 export default {
-	data() {
-		return {
-			active: '',
-		}
-	},
-	components: {
-		StaffMenu
-	}
-}
+  data() {
+    return {
+      active: "",
+    };
+  },
+  components: {
+    StaffMenu,
+    ItemMenu,
+  },
+};
 </script>
