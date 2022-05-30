853972f87c61e6d29af944de63aac4f2295258f4
ericsinger <erics118@educbe.ca>
2022-05-30 15:08:55 -0600

    fix chrome kiosk

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,3 @@
 cd /docker/jcstab/docker && docker-compose up -d --force-recreate
 sleep 1m
-chromium-browser --kiosk http://localhost:5000
+chromium-browser --noerrdialogs --disable-infobars --start-fullscreen http://localhost:5000
