aeaa9c35bf14b681719402715cb322750354e993
BuildTools <unconfigured@null.spigotmc.org>
2022-06-05 17:39:52 -0600

    add a check if SW got registered

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,18 @@
 <script>
 export default {
   name: "App",
+  mounted() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((reg) => {
+          console.log("Registration succesful, scope: " + reg.scope);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  },
 };
 </script>
 
