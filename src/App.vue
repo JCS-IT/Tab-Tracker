dba2c1f56732dc5b788744df9dd0d693689338e8
BuildTools <unconfigured@null.spigotmc.org>
2022-06-05 18:24:48 -0600

    remove the SW check

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,6 @@
 <script>
 export default {
   name: "App",
-  mounted() {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then((reg) => {
-          console.log("Registration succesful, scope: " + reg.scope);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  },
 };
 </script>
 
