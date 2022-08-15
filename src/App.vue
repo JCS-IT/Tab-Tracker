08d45ce5573d04db87f45592f17b6e77d2149e83
BuildTools <unconfigured@null.spigotmc.org>
2022-08-15 14:35:38 -0600

    begin rewrite to ts and use of cloud functions

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar color="primary">
-      <v-toolbar-title>JCS Tabs</v-toolbar-title>
-      <v-spacer />
-      <user-menu v-if="loggedIn" :user="user" @logout="logout()"></user-menu>
-    </v-app-bar>
     <v-main>
-      <router-view />
-      <div id="firebaseui-auth-container"></div>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-import { auth } from "./firebase";
-import { GoogleAuthProvider } from "@firebase/auth";
-import * as firebaseui from "firebaseui";
-import UserMenu from "./components/UserMenu.vue";
+<script lang="ts">
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "App",
-  components: { UserMenu },
-  data() {
-    return {
-      loginMenu: false,
-      loggedIn: false,
-      user: {},
-    };
-  },
-  methods: {
-    showAuthMenu() {
-      let ui = firebaseui.auth.AuthUI.getInstance();
-      if (!ui) {
-        ui = new firebaseui.auth.AuthUI(auth);
-      }
-      const uiConfig = {
-        signinFlow: "popup",
-        signInSuccessUrl: "/",
-        signInOptions: [
-          {
-            provider: GoogleAuthProvider.PROVIDER_ID,
-            clientID:
-              "557593235569-a0jimberq7k4j3s90klgju18msi1fel0.apps.googleusercontent.com",
-          },
-        ],
-        credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
-      };
-      ui.start("#firebaseui-auth-container", uiConfig);
-    },
-    logout() {
-      auth.signOut();
-      this.$router.push("/");
-      this.loggedIn = false;
-    },
-  },
-  mounted() {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        this.loggedIn = true;
-        this.user = {
-          name: user.displayName,
-          email: user.email,
-          photoURL: user.photoURL,
-        };
-      } else {
-        this.loggedIn = false;
-        this.showAuthMenu();
-      }
-    });
-  },
-});
-</script>
+  name: 'App',
 
-<style>
-* {
-  text-decoration: none;
-}
-</style>
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
