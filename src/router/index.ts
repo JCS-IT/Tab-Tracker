d7f0e07b6ade2df00a78611329bc0a4836d565ed
BuildTools <unconfigured@null.spigotmc.org>
2022-08-29 16:53:33 -0600

    fix build step

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ const router = createRouter({
     {
       path: "/login",
       name: "Login",
-      component: () => import("../views/login.vue"),
+      component: () => import("../views/Login.vue"),
     },
     {
       path: "/user",
