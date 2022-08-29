b856b457b85a0ef3938ef503dc9981b5a1d62f6d
BuildTools <unconfigured@null.spigotmc.org>
2022-08-29 11:03:43 -0600

    add admin toggle

--- src/components/admin/menus/StaffMenu.vue
+++ src/components/admin/menus/StaffMenu.vue
@@ -1,8 +1,34 @@
 <template>
   <v-container>
     <v-row>
-      <v-expansion-panels>
-        <v-expansion-panel v-for="letter in letters" :key="letter">
+      <v-col width="100px">
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-text-field
+              v-model="search"
+              placeholder="Search"
+              v-bind="props"
+            />
+          </template>
+          <v-list>
+            <v-list-item v-for="(user, index) in searchForUser" :key="index">
+              <v-list-item-title @click="getUser(user)">
+                {{ user.data.displayName }}
+              </v-list-item-title>
+              <v-divider />
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-expansion-panels v-model="panels" multiple>
+        <v-expansion-panel
+          v-for="letter in letters"
+          :key="letter"
+          :value="letter"
+          :id="letter"
+        >
           <v-expansion-panel-title>
             <b>{{ letter.toUpperCase() }}</b>
           </v-expansion-panel-title>
@@ -13,36 +39,53 @@
               :key="index"
             >
               <template v-slot:activator="{ props }">
-                <v-btn icon v-bind="props" @click="dialog[index] = true">
+                <v-btn
+                  icon
+                  v-bind="props"
+                  @click="dialog[user.data.uid] = true"
+                >
                   <v-avatar>
                     <v-img :src="user?.data.photoURL" alt="Avatar" />
                   </v-avatar>
                 </v-btn>
-                <v-dialog v-model="dialog[index]">
-                  <v-card width="200px">
+                <v-dialog v-model="dialog[user.data.uid]" persistent>
+                  <v-card>
                     <v-card-title>
                       <v-avatar>
                         <v-img :src="user?.data.photoURL" alt="Avatar" />
                       </v-avatar>
                       <span>{{ user.data.displayName }}</span>
                     </v-card-title>
+                    <v-divider />
                     <v-card-text>
+                      <h4>Tab:</h4>
                       <v-list>
                         <v-list-item
                           v-for="(item, index) in items"
                           :key="index"
                         >
-                          <v-list-item-title>{{ item }}</v-list-item-title>
-                          <v-list-item-subtitle>
+                          <v-list-item-title>
+                            {{ item }}s:
                             {{ total(item, user) }}
-                          </v-list-item-subtitle>
+                          </v-list-item-title>
                         </v-list-item>
                       </v-list>
+                    </v-card-text>
+                    <v-divider />
+                    <v-card-text>
+                      <h4>Roles:</h4>
+                      <v-switch
+                        :loading="loading"
+                        :disabled="loading"
+                        :model-value="user.roles.admin"
+                        label="Administrator"
+                        @click="toggleAdmin(user)"
+                      />
                     </v-card-text>
                     <v-card-actions>
                       <v-btn
                         color="primary"
-                        @click="dialog[index] = false"
+                        @click="dialog[user.data.uid] = false"
                         text
                       >
                         Close
@@ -63,8 +106,9 @@
 
 <script lang="ts">
 import { defineComponent, defineAsyncComponent } from "vue";
-import { auth, db } from "@/firebase";
+import { auth, db, functions } from "@/firebase";
 import { doc, collection, onSnapshot, Timestamp } from "firebase/firestore";
+import { httpsCallable } from "@firebase/functions";
 let userSub: () => void;
 let itemSub: () => void;
 
@@ -81,6 +125,10 @@ interface User {
       name: string;
     }
   ];
+  roles: {
+    admin: boolean;
+    dev: boolean;
+  };
 }
 
 interface Dialog {
@@ -90,7 +138,6 @@ interface Dialog {
 export default defineComponent({
   components: {
     DeleteUser: defineAsyncComponent(() => import("../prompts/DeleteUser.vue")),
-    User: defineAsyncComponent(() => import("../components/User.vue")),
     ClearTab: defineAsyncComponent(() => import("../prompts/ClearTab.vue")),
   },
   data() {
@@ -98,6 +145,9 @@ export default defineComponent({
       dialog: {} as Dialog,
       users: [] as User[],
       items: [] as string[],
+      search: "" as string,
+      panels: [] as string[],
+      loading: false as boolean,
       letters: [
         "a",
         "b",
@@ -128,6 +178,15 @@ export default defineComponent({
       ],
     };
   },
+  computed: {
+    searchForUser() {
+      return this.users.filter((user: any) => {
+        return user.data.displayName
+          .toLowerCase()
+          .includes(this.search.toLowerCase());
+      }) as User[];
+    },
+  },
   methods: {
     filterUsers(letter: string) {
       return this.users?.filter((user: User) => {
@@ -141,6 +200,30 @@ export default defineComponent({
       return user.tab.filter((tab: { name: string }) => {
         return tab.name === item;
       }).length;
+    },
+    getUser(user: User) {
+      this.panels = [];
+      const panel = user.data.displayName.split(" ")[1].toLowerCase().charAt(0);
+      this.panels.push(panel);
+      const element = document.getElementById(panel);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+      this.dialog[user.data.uid] = true;
+    },
+    async toggleAdmin(user: User) {
+      this.loading = true;
+      try {
+        const toggleRole = httpsCallable(functions, "toggleAdmin");
+        await toggleRole({
+          uid: user.data.uid,
+          email: user.data.email,
+          admin: !user.roles.admin,
+        });
+      } catch (err) {
+        this.$emit("error", err);
+      }
+      this.loading = false;
     },
   },
   beforeDestroy() {
