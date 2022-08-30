dc2fc5559e5104aadb35fe5493de46f469ee7fd6
BuildTools <unconfigured@null.spigotmc.org>
2022-08-29 21:10:52 -0600

    secure the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import { auth } from "@/firebase";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
@@ -17,13 +18,40 @@ const router = createRouter({
       path: "/user",
       name: "User",
       component: () => import("../views/User.vue"),
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/admin",
       name: "Admin",
       component: () => import("../views/Admin.vue"),
+      meta: {
+        requiresAuth: true,
+        requiresAdmin: true,
+      },
     },
   ],
 });
 
 export default router;
+
+router.beforeEach(async (to, from, next) => {
+  if (import.meta.env.DEV) {
+    return next();
+  }
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  const requiresAdmin = to.matched.some((record) => record.meta.requiresAdmin);
+  const user = await auth.currentUser;
+  const admin = (await auth.currentUser?.getIdTokenResult())?.claims.admin;
+  switch (true) {
+    case requiresAuth && !user:
+      return next({
+        name: "Login",
+      });
+    case requiresAdmin && !admin:
+      return next(from.path);
+    default:
+      return next();
+  }
+});
