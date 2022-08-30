a9c8e2d4ff8bd735a7eddfc261db28e221f87e47
BuildTools <unconfigured@null.spigotmc.org>
2022-08-30 11:38:18 -0600

    add functions emulator to dev env

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getFunctions } from "firebase/functions";
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 import { getAnalytics } from "firebase/analytics";
 import { getPerformance } from "firebase/performance";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
@@ -27,5 +27,9 @@ initializeAppCheck(app, {
   provider: new ReCaptchaV3Provider("6LfgHEEgAAAAAEaYmNJkZHGvxQ4-c6syHPdOb5r5"),
   isTokenAutoRefreshEnabled: true,
 });
+
+if (import.meta.env.DEV) {
+  connectFunctionsEmulator(functions, "localhost", 5001);
+}
 
 export { app, auth, db, functions };
