53bc0764a7ae51a6af1c5daa51d43f2a722bb245
BuildTools <unconfigured@null.spigotmc.org>
2022-09-27 16:54:56 -0600

    update packages

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ import vuetify from "vite-plugin-vuetify";
 import { VitePWA } from "vite-plugin-pwa";
 import mkcert from "vite-plugin-mkcert";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
