25bf383f3821acda371780ac36693fb7fda52077
BuildTools <unconfigured@null.spigotmc.org>
2022-10-18 17:56:09 -0600

    overhaul all the things

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,64 @@
-<template>
-  <VApp>
-    <VToolbar color="blue-lighten-2">
-      <VToolbarTitle> JCS Tabs </VToolbarTitle>
-    </VToolbar>
-    <VMain>
-      <RouterView />
-    </VMain>
-    <VFooter app color="blue-lighten-2" height="64px">
-      <VContainer>
-        <VRow justify="center" no-gutters>
-          <FeedBack />
-        </VRow>
-      </VContainer>
-    </VFooter>
-  </VApp>
-</template>
-
 <script setup lang="ts">
-import { defineAsyncComponent } from "vue";
+import { ref, defineAsyncComponent } from "vue";
 import { auth } from "utils/firebase";
 import { useRouter } from "vue-router";
+
+const router = useRouter();
+const loggedIn = ref(false);
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    router.push("/user");
+    loggedIn.value = true;
+  } else {
+    router.push("/login");
+    loggedIn.value = false;
+  }
+});
 
 const FeedBack = defineAsyncComponent(
   () => import("@/components/FeedBack.vue")
 );
 
-const router = useRouter();
-
-auth.onAuthStateChanged((user) => {
-  if (user) {
-    router.push("/user");
-  } else {
-    router.push("/login");
-  }
-});
+const UserProfile = defineAsyncComponent(
+  () => import("@/components/UserProfile.vue")
+);
 </script>
+
+<template>
+  <VApp>
+    <VAppBar color="blue-lighten-2">
+      <VAppBarNavIcon @click="$router.push({ name: 'Home' })" icon="mdi-home" />
+      <VAppBarTitle> JCS Tabs </VAppBarTitle>
+      <UserProfile v-if="loggedIn" />
+    </VAppBar>
+    <VMain>
+      <RouterView v-slot="{ Component }">
+        <Transition name="fade" mode="out-in">
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
+    </VMain>
+    <VFadeTransition v-if="loggedIn">
+      <VFooter app color="blue-lighten-2" height="64px">
+        <VContainer>
+          <VRow justify="center" no-gutters>
+            <FeedBack />
+          </VRow>
+        </VContainer>
+      </VFooter>
+    </VFadeTransition>
+  </VApp>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
