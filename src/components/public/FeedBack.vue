b6b043393d72ced1d45b4e2072650e2641adbde4
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 17:07:03 -0600

    backport feedback submission

--- src/components/public/FeedBack.vue
+++ src/components/public/FeedBack.vue
@@ -1,40 +1,43 @@
 <template>
-  <VDialog v-model="dialog" max-width="500px">
-    <template v-slot:activator="{ props }">
-      <VBtn v-bind="props" color="green-lighten-2" @click="dialog = true">
+  <v-dialog v-model="dialog" max-width="500px">
+    <template #activator="{ props }">
+      <v-btn v-bind="props" color="green-lighten-2" @click="dialog = true">
         Feedback
-      </VBtn>
+      </v-btn>
     </template>
-    <VCard :loading="loading">
-      <VAlert
-        v-if="error"
+    <v-card :loading="loading">
+      <v-alert
+        v-if="error.code"
         type="error"
         dismissible
         transition="scale-transition"
         elevation="2"
         color="red"
       >
-        {{ error }}
-      </VAlert>
-      <VCardTitle class="headline">Feedback Submission</VCardTitle>
-      <VCardText>
-        <VForm ref="input">
-          <VTextarea
-            v-model="input.text"
+        <v-alert-title>
+          {{ error.code }}
+        </v-alert-title>
+        {{ error.message }}
+      </v-alert>
+      <v-card-title class="headline">Feedback Submission</v-card-title>
+      <v-card-text>
+        <v-form ref="inputForm">
+          <v-textarea
+            v-model="input"
             label="Feedback"
             placeholder="Enter your feedback here..."
             outlined
             rows="5"
             :rules="rules.text"
           />
-        </VForm>
-      </VCardText>
-      <VCardActions>
-        <VBtn color="green-lighten-2" text @click="submit">Submit</VBtn>
-        <VBtn color="error" text @click="dialog = false">Cancel</VBtn>
-      </VCardActions>
-    </VCard>
-  </VDialog>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="green-lighten-2" text @click="submit">Submit</v-btn>
+        <v-btn color="error" text @click="dialog = false">Cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
@@ -43,36 +46,60 @@ import { auth, db } from "@/firebase";
 import { addDoc, Timestamp, collection } from "firebase/firestore";
 
 const dialog = ref(false);
-const input = ref({
-  text: "",
-});
+
+const input = ref("");
+
+const inputForm = ref(null);
+
 const loading = ref(false);
-const error = ref("");
+
+const error = ref({
+  code: null,
+  message: null,
+} as { code: string | null; message: string | null });
+
 const rules = {
   text: [(v: string) => !!v || "Field cannot be empty"],
 };
-
 const submit = async () => {
   // @ts-expect-error
-  const inputValidation = await input.value?.validate();
-  if (inputValidation.valid === false) return;
+  const { valid } = await inputForm.value.validate();
+
+  if (!valid) return;
+
   loading.value = true;
+
   try {
     await addDoc(collection(db, "feedback"), {
       name: auth.currentUser?.displayName,
       email: auth.currentUser?.email,
       date: Timestamp.now(),
-      text: input.value.text,
+      text: input.value,
+      version: {
+        app: import.meta.env.VITE_APP_VERSION,
+        mode: import.meta.env.MODE,
+      },
     });
-    input.value.text = "";
-    error.value = "";
-    dialog.value = false;
+
+    close();
   } catch (err) {
-    // @ts-ignore
-    error.value = err.message as string;
+    const { code, message } = err as { code: string; message: string };
+    error.value = { code, message };
+
     console.log(error);
   } finally {
     loading.value = false;
   }
+};
+
+const close = () => {
+  input.value = "";
+
+  error.value = {
+    code: null,
+    message: null,
+  };
+
+  dialog.value = false;
 };
 </script>
