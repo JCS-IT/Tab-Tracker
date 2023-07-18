3e2e020e393b032b200caa4009fec2d02009e5c3
LeaderbotX400 <eric@singerfamily.ca>
2023-07-18 08:10:24 -0600

    fix: plugins

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,22 +1,7 @@
-import { registerSW } from "virtual:pwa-register";
 import { createApp } from "vue";
 
 import App from "./App.vue";
-import { registerPlugins } from "./plugins";
-
-const intervalMS = 60 * 60 * 1000;
-
-registerSW({
-  onRegistered(r) {
-    r &&
-      setInterval(() => {
-        r.update();
-      }, intervalMS);
-  },
-  onNeedRefresh() {
-    window.location.reload();
-  },
-});
+import { registerPlugins } from "./modules";
 
 const app = createApp(App);
 
