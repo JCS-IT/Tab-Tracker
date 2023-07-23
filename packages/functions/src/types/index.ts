9600ad8622fdbf4dbe15ebe0b6a3dac6d7ed6786
LeaderbotX400 <eric@singerfamily.ca>
2023-07-23 16:05:08 -0600

    fix shared types

--- packages/functions/src/types/index.ts
+++ packages/functions/src/types/index.ts
@@ -1,19 +1,13 @@
 import { Timestamp } from "firebase-admin/firestore";
 import { CallableRequest } from "firebase-functions/v2/https";
+import { TabItem as SharedTabItem, Item as SharedItem } from "@jcstabs/shared";
 
 export interface Props<T> extends CallableRequest {
   data: T;
 }
 
-export interface TabItem {
-  name: string;
-  price: number;
+export interface TabItem extends Omit<SharedTabItem, "date"> {
   date: Timestamp;
-  paid: boolean;
-  clearedBy?: string;
 }
 
-export interface Item {
-  name: string;
-  price: number;
-}
+export interface Item extends SharedItem {}
