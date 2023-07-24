2b2925c61fc2b1dc7a25eb25efd57f3bb31e49e7
LeaderbotX400 <eric@singerfamily.ca>
2023-07-24 12:50:22 -0600

    chore: cleanup

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -2,15 +2,8 @@ import { initializeApp } from "firebase-admin/app";
 
 initializeApp();
 
-// User functions
-export { clearTab, clearHistory, toggleRole } from "./user";
-
-// Item functions
-export { addItem, deleteItem, updateItem } from "./items";
-
-// Auth functions
-export { beforeCreate, onCreate, onDelete } from "./auth";
-
-// FCM functions
-export { updateToken, updateTopics } from "./fcm/token";
-export { itemsUpdated } from "./fcm/notifications";
+export * from "./user";
+export * from "./items";
+export * from "./auth";
+export * from "./fcm/token";
+export * from "./fcm/notifications";
